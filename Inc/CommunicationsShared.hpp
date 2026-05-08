4cd271b1350d005fe6b623e7b19166485cb1bf85
FoniksFox <borisbeslimov@gmail.com>
2026-05-08 09:56:50 +0200

    More logs

--- Inc/CommunicationsShared.hpp
+++ Inc/CommunicationsShared.hpp
@@ -33,6 +33,7 @@ constexpr CommandFlags operator~(CommandFlags a) {
 
 struct LevitateParams {
     float desired_distance;
+    bool ramping;
 };
 
 struct CurrentControlParams {
@@ -64,7 +65,7 @@ struct CommandPacket {
     CommandPacket() 
         : start_byte(START_BYTE)
         , flags(CommandFlags::NONE)
-        , levitate{0.0f}
+        , levitate{0.0f, false}
         , current_control{0.0f, 0}
         , force_enable_lpu_buffer{0}
         , end_byte(END_BYTE)
@@ -99,6 +100,21 @@ struct StatusPacket {
     float airgap_local_3;
     float airgap_local_4;
 
+    float Fe[3];
+    float Fa[4];
+    float Ef[3];
+    float P[3];
+    float R[3];
+    float Zz[3];
+    float Fe_L[3];
+
+    float desired_voltage_1;
+    float desired_voltage_2;
+    float desired_voltage_3;
+    float desired_voltage_4;
+
+    float target_distance;
+
     SlaveState slave_state;   // SystemStates enum value
     uint16_t error_code;            // Detailed error code if fault
 
@@ -119,6 +135,18 @@ struct StatusPacket {
         , airgap_local_2(0.0f)
         , airgap_local_3(0.0f)
         , airgap_local_4(0.0f)
+        , target_distance(0.0f)
+        , Fe{0.0f, 0.0f, 0.0f}
+        , Fa{0.0f, 0.0f, 0.0f, 0.0f}
+        , Ef{0.0f, 0.0f, 0.0f}
+        , P{0.0f, 0.0f, 0.0f}
+        , R{0.0f, 0.0f, 0.0f}
+        , Zz{0.0f, 0.0f, 0.0f}
+        , Fe_L{0.0f, 0.0f, 0.0f}
+        , desired_voltage_1(0.0f)
+        , desired_voltage_2(0.0f)
+        , desired_voltage_3(0.0f)
+        , desired_voltage_4(0.0f)
         , slave_state(SlaveState::SPI_CONNECTING)
         , error_code(0)
         , end_byte(END_BYTE)
